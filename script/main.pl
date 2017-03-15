57dd09420add45c034df6efe8bff27cdc4ebeb7c
ShassBeleth <kawachi.shusaku@opensesame-tech.com>
2017-03-15 18:53:12 +0900

    要求を受け取ってリアクションするようにした

--- script/main.pl
+++ script/main.pl
@@ -20,10 +20,22 @@ use utf8;
 my $conf_file = "../configuration.ini";
 my $config = Config::Tiny->new->read( $conf_file );
 
-#接続情報
+#接続情報取得
 my $http_daemon = HTTP::Daemon->new( LocalAddr => '0.0.0.0' , LocalPort => $config->{AccessPoint}->{Port} ) || die;
 my $url = $http_daemon->url;
 
-print $url."\n";
-
-&debug_log( "やったぜ" );
+&info_log( "接続受付開始\n" );
+while( my ( $client_connection , $peer_addr ) = $http_daemon->accept ){
+	
+	#要求を受け付けた時
+	while( my $request = $client_connection->get_request ){
+	
+		#要求を出したクライアントのIPアドレスを取得
+		my ( $port , $ip ) = sockaddr_in( $peer_addr );
+		$ip = join( "." , unpack( "C4" , $ip ) );
+		
+		&info_log( $ip."からの要求を受け取りました" );
+	
+	}
+	
+}
